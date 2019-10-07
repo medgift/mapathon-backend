b69216713569336dd531dc6ba0b4e2dac1986b1b
Roger Schaer <roger.schaer@hevs.ch>
2019-10-07 15:01:03 +0200

    Set TTL for Auth0 API Cache

--- auth0/cache.js
+++ auth0/cache.js
@@ -1,5 +1,5 @@
 const NodeCache = require("node-cache");
-const cache = new NodeCache();
+const cache = new NodeCache({ stdTTL: 600 });
 
 // route middleware to cache calls to the Auth0 management API (for user info, which doesn't change frequently)
 module.exports = cache;
